4c748d29166d052cdc8b16a400e9b2465c667eaa
Yuki Okada <okada126@gmail.com>
2023-12-20 14:26:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <?php
         ini_set('date.timezone', 'Asia/Tokyo');
         echo nl2br("Current time：".date('h:i:s') ."\n");
-        sleep(10);
-        echo "Time at 10 seconds later:".date('h:i:s');
+        sleep(40);
+        echo "Time at 40 seconds later:".date('h:i:s');
     ?>
 </body>
 </html>
